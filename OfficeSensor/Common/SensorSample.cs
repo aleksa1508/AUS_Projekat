4486493e40a69c730bf4118a0a1036d4959918df
aleksa1508 <aleksaarsenic15.08@gmail.com>
2025-09-13 13:12:04 +0200

    common i service class

---
+++ OfficeSensor/Common/SensorSample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class SensorSample
+    {
+        [DataMember]
+        public double Volume { get; set; }
+        [DataMember]
+        public double RelativeHumidity {  get; set; }
+        [DataMember]
+        public double AirQuality { get; set; }
+        [DataMember]
+        public double LightLevel { get; set; }
+        [DataMember]
+        public DateTime DateTime { get; set; }
+    }
+}
